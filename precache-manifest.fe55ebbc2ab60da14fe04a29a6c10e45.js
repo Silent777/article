f01e9ce30ff2631573c63543164b7e5978b9d9ef
silent777 <silentismyfriend@gmail.com>
2019-01-05 19:30:12 +0200

    Updates

---
+++ precache-manifest.fe55ebbc2ab60da14fe04a29a6c10e45.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "52d983c666b7db9c6c59",
+    "url": "/article/static/css/main.3ff11da3.chunk.css"
+  },
+  {
+    "revision": "52d983c666b7db9c6c59",
+    "url": "/article/static/js/main.52d983c6.chunk.js"
+  },
+  {
+    "revision": "3c3ec2e030522ee4c59c",
+    "url": "/article/static/js/1.3c3ec2e0.chunk.js"
+  },
+  {
+    "revision": "597033346b0b3baca4ec",
+    "url": "/article/static/js/runtime~main.59703334.js"
+  },
+  {
+    "revision": "96c69156e39bf7563419610ffd0375b9",
+    "url": "/article/index.html"
+  }
+];
